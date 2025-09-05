0a6a3d10f47504b96e54157cc9c15cb05e5ae487
TristanEhm <canidile@gmail.com>
2025-09-04 21:48:03 -0400

    Initial Commit

---
+++ src/client/java/github/tristanehm/multicutter/MultiCutterDataGenerator.java
@@ -0,0 +1,11 @@
+package github.tristanehm.multicutter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MultiCutterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
